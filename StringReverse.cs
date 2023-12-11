f4e42873f55d29c412a8116d1f70f55098f136af
DBsMOJO <d.binder1@students.htl-leonding.ac.at>
2023-12-11 19:18:28 +0100

    add code

---
+++ StringReverse.cs
@@ -0,0 +1,36 @@
+﻿/*--------------------------------------------------------------
+ *				HTBLA-Leonding / Class: 3.ACIF
+ *--------------------------------------------------------------
+ *              Daniel Binder, 11.12.2023
+ *--------------------------------------------------------------
+ * Description:
+ * ...
+ *--------------------------------------------------------------
+*/
+
+using System;
+
+namespace StringReverse
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string input,
+                output = string.Empty;
+
+            // Input
+            Console.Write("Geben Sie einen Text ein: ");
+            input = Console.ReadLine();
+
+            // Processing
+            for (int i = input.Length; i > 0; --i)
+            {
+                output += input[i - 1];
+            }
+
+            // Output
+            Console.WriteLine(output);
+        }
+    }
+}
